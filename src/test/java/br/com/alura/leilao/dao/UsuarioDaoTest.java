fabaaad43701dbd62a72e15e3923d93a01ea5752
Daniel Lopes <viana.daniel02@gmail.com>
2022-01-22 15:37:02 -0300

    test: adicionado testes de integração para o método buscarUsuarioPorNome.

--- src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -1,14 +1,32 @@
 package br.com.alura.leilao.dao;
 
+import static org.junit.Assert.assertNotNull;
+
+import javax.persistence.EntityManager;
+
 import org.junit.jupiter.api.Test;
+
+import br.com.alura.leilao.model.Usuario;
+import br.com.alura.leilao.util.JPAUtil;
 
 
 
 class UsuarioDaoTest {
 	
+	private UsuarioDao usuDao;
+	
 	@Test
 	void testeBuscarUsuarioPeloUsername() {
+		EntityManager em = JPAUtil.getEnitiEntityManager();
+		this.usuDao = new UsuarioDao(em);
 		
+		Usuario usuario = new Usuario("Cicrano", "cicrano@gmail.com", "12345");
+		em.getTransaction().begin();
+		em.persist(usuario);
+		em.getTransaction().commit();
+		
+		Usuario usuarioEncontrado = this.usuDao.buscarPorUsername(usuario.getNome());
+		assertNotNull(usuarioEncontrado);
 	}
 
 }
