0c54e1bbad0900a9cbda16c439617e02fb29e17e
Daniel Lopes <viana.daniel02@gmail.com>
2022-01-23 20:42:59 -0300

    refactor: organizando e melhorando a class de teste UsuarioDaoTest.java

--- src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.alura.leilao.model.Usuario;
@@ -17,34 +19,42 @@ import br.com.alura.leilao.util.JPAUtil;
 class UsuarioDaoTest {
 	
 	private UsuarioDao usuDao;
+	private EntityManager em;
+	
+	@BeforeEach
+	public void beforeEach() {
+		this.em = JPAUtil.getEnitiEntityManager();
+		this.usuDao = new UsuarioDao(em);
+		em.getTransaction().begin();
+	}
+	
+	@AfterEach
+	public void afterEach() {
+		em.getTransaction().rollback();
+	}
+	
 	
 	@Test
 	void deveriaEncontrarUsuarioCadastrado() {
-		EntityManager em = JPAUtil.getEnitiEntityManager();
-		this.usuDao = new UsuarioDao(em);
-		
-		Usuario usuario = new Usuario("Cicrano", "cicrano@gmail.com", "12345");
-		em.getTransaction().begin();
-		em.persist(usuario);
-		em.getTransaction().commit();
-		
+		Usuario usuario = criarUsuario();
 		Usuario usuarioEncontrado = this.usuDao.buscarPorUsername(usuario.getNome());
+
 		assertNotNull(usuarioEncontrado);
 	}
 
 	@Test
 	void naoDeveriaEncontrarUsuarioCadastrado() {
-		EntityManager em = JPAUtil.getEnitiEntityManager();
-		this.usuDao = new UsuarioDao(em);
-		
-		Usuario usuario = new Usuario("Fulano", "fulano@gmail.com", "123456");
-		em.getTransaction().begin();
-		em.persist(usuario);
-		em.getTransaction().commit();
+		criarUsuario();
 		
 		assertThrows(NoResultException.class, () -> this.usuDao.buscarPorUsername("Beltrano"));
 	}
 	
-	
+	private Usuario criarUsuario(){
+		Usuario usuario = new Usuario("Fulano", "fulano@gmail.com", "123456");
+
+		em.persist(usuario);
+		
+		return usuario;
+	}
 
 }
